f4d5c7becf79522cfa185bf0142abea276776778
froZin <bolutolu12@gmail.com>
2024-09-18 02:21:33 +0100

    validated the input

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
-const foods = document.getElementById("foods");
+const form = document.getElementById("form");
 
-const objects = {
-    username: `Mike`,
-    best: "indigo",
-}
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
 
-console.log(objects.username);
+    let emailInput = document.getElementById("email");
+    let checker = document.body.querySelector(".email")
+    let errorMsg = document.getElementById("invalid");
+    let emailPattern =  /^([a-zA-Z0-9-\.])+@([a-zA-Z0-9-\.])+\.([a-z]{2,8})(\.[a-z]{2,5})?$/;
+
+    console.log()
+
+    if(emailInput.value.match(emailPattern)){
+        checker.classList.remove("error");
+        emailInput.style.border = '1.5px solid green'
+        errorMsg.textContent = "Email is valid";
+        errorMsg.style.display = "block"
+        errorMsg.style.color = "green"
+    }
+    else {
+        checker.classList.add("error");
+        errorMsg.style.display = "block"
+    }
+
+})
